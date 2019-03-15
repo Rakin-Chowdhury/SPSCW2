197a63573a1fcb346014dbb8ec6efbc04243a8a7
Rakin Chowdhury <Rc16453@bristol.ac.uk>
2019-03-15 17:09:11 +0000

    lab 5 part 1

--- Coursework_2/utilities.py
+++ Coursework_2/utilities.py
@@ -1,13 +1,14 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
-def load_data(train_set_path='data/wine_train.csv', 
-              train_labels_path='data/wine_train_labels.csv', 
+def load_data(train_set_path='data/wine_train.csv',
+              train_labels_path='data/wine_train_labels.csv',
               test_set_path='data/wine_test.csv',
               test_labels_path='data/wine_test_labels.csv'):
     """
     Loads the wine dataset. If no arguments are passed it will try to load the data
     from the working directory with the default file names
-    
+
     Args:
         train_set_path : path to the train set .csv file
         train_labels_path : path to the train labels .csv file
@@ -17,12 +18,12 @@ def load_data(train_set_path='data/wine_train.csv',
         (train_set, train_labels, test_set, test_labels), numpy arrays containing the
         training and testing sets, along with the respective class labels
     """
-    
+
     train_set = np.loadtxt(train_set_path, delimiter=',')
     train_labels = np.loadtxt(train_labels_path, delimiter=',')
     test_set = np.loadtxt(test_set_path, delimiter=',')
     test_labels = np.loadtxt(test_labels_path, delimiter=',', dtype=np.int)
-    
+
     return train_set, train_labels, test_set, test_labels
 
 
@@ -30,39 +31,39 @@ def print_predictions(predictions):
     """
     Prints the classifier predictions to the standard output in the format expected
     by the auto-marker.
-    
-    Args: 
-        predictions: can be either a list or a NumPy array. 
-        If your predictions are an np.array, then the array must be either 1D or 
+
+    Args:
+        predictions: can be either a list or a NumPy array.
+        If your predictions are an np.array, then the array must be either 1D or
         have shape (n, 1) or (1, n),
         If your predictions are a list, then it must be a 1D list
     """
     _print_for_automaker(predictions, 'predictions')
-    
+
 
 def print_features(features):
     """
     Prints the selected features to the standard output in the format expected
     by the auto-marker.
-    
-    Args: 
-        features: can be either a list or a NumPy array. 
-        If your features are an np.array, then the array must be either 1D or 
+
+    Args:
+        features: can be either a list or a NumPy array.
+        If your features are an np.array, then the array must be either 1D or
         have shape (n, 1) or (1, n),
         If your features are a list, then it must be a 1D list
     """
     _print_for_automaker(features, 'features')
 
 
-def _print_for_automaker(D, what):    
+def _print_for_automaker(D, what):
     """
-    Internal function for printing things for the auto-marker. 
-    You should not use this function. 
+    Internal function for printing things for the auto-marker.
+    You should not use this function.
     Use either `print_predictions` or `print_features`
     """
     p = None
     t = type(D)
-    
+
     if t is np.ndarray:
         assert D.ndim == 1 or (D.ndim == 2 and min(D.shape) == 1), \
         'If your {} are an np.array, then the array must be either 1D or have shape (n, 1) or (1, n). Your shape is {}'.format(what, D.shape)
@@ -74,5 +75,22 @@ def _print_for_automaker(D, what):
         p = D
     else:
         raise Exception('{} should be passed as numpy array or list. Your predictions were of type {}'.format(what, t))
-        
+
     print(p)
+d = load_data(train_set_path='data/wine_train.csv',
+              train_labels_path='data/wine_train_labels.csv',
+              test_set_path='data/wine_test.csv',
+              test_labels_path='data/wine_test_labels.csv')
+train = d[0]
+label = d[1]
+#print(train[0][:])
+
+c1 = []
+c2 = []
+for j in range(76):
+    for i in range(52):
+        c1.append(train[i][j])
+        c2.append(train[i][j])
+
+plt.scatter(c1,c2)
+plt.show()
