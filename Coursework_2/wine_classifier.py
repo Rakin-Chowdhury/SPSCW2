b7e686e2c96a58a4309ab0a533b3ad3bd25cb3a5
Rakin Chowdhury <Rc16453@bristol.ac.uk>
2019-04-30 12:49:02 +0100

    as of 30 apr

--- Coursework_2/wine_classifier.py
+++ Coursework_2/wine_classifier.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Skeleton code for CW2 submission.
-We suggest you implement your code in the provided functions
-Make sure to use our print_features() and print_predictions() functions
-to print your results
-"""
 
 from __future__ import print_function
 
@@ -21,8 +15,33 @@ CLASS_2_C = r'#cc3300'
 CLASS_3_C = r'#ffc34d'
 class_colours = [CLASS_1_C, CLASS_2_C, CLASS_3_C]
 
+#doug smells
+
 
 MODES = ['feature_sel', 'knn', 'alt', 'knn_3d', 'knn_pca']
+
+def calculate_accuracy(gt_labels,pred_labels):
+    counter=0
+    length=len(pred_labels) #same as RES
+    for i in range(length):
+        if gt_labels[i]!=pred_labels[i]:
+            counter+=1
+            #finds the number of predicted classes that are wrong
+        Acc=1-(counter/length) #how accurate classifer is as a %/100
+    print(Acc)
+    return Acc
+
+def entropy(partition1i):
+    p1ilength=np.size(partition1i)
+    unique,counts = np.unique(partition1i, return_counts=True)
+    assignment=unique[np.where(counts == max(counts))][0]
+    i=0
+    p1ientr=0
+    while i < np.size(counts):
+        p1ientr+=((-(counts[i]/p1ilength)*np.log(counts[i]/p1ilength)))
+        i+=1
+    p1ientr=1-p1ientr
+    return unique,counts,assignment,p1ientr
 
 def plotSelFeatures(feature1, feature2, labels):
     Class_Colours= [CLASS_1_C, CLASS_2_C, CLASS_3_C]
@@ -36,6 +55,10 @@ def plotSelFeatures(feature1, feature2, labels):
     plt.show()
 
     return
+
+def conMat(Real, predicted):
+
+    return 0
 
 def feature_selection(train_set, train_labels, **kwargs):
 
@@ -79,7 +102,7 @@ def feature_selection(train_set, train_labels, **kwargs):
 
     return [fe]
 
-def KnnNeigh(reducedTrainSet, trainLabels, reducedTestSet, k):
+def KnnNeigh(reducedTrainSet, trainLabels, reducedTestSet, k, d):
 
     predTestLabels = []
     for testData in reducedTestSet:
@@ -87,7 +110,16 @@ def KnnNeigh(reducedTrainSet, trainLabels, reducedTestSet, k):
         neighboursClass = []
 
         for i, trainData in enumerate(reducedTrainSet):
-            EuDistance = np.sqrt((testData[0] - trainData[0])**2 + (testData[1] - trainData[1])**2)
+
+
+            if d == 2:
+                EuDistance = np.sqrt((testData[0] - trainData[0])**2 + (testData[1] - trainData[1])**2)
+            elif d == 3:
+                EuDistance = np.sqrt((testData[0] - trainData[0])**2 + (testData[1] - trainData[1])**2  + (testData[2] - trainData[2])**2)
+
+
+
+
             neighbours.append([EuDistance, i])
 
 
@@ -99,7 +131,13 @@ def KnnNeigh(reducedTrainSet, trainLabels, reducedTestSet, k):
             neighboursClass.append(trainLabels[n[1]])
 
 
-        predTestLabels.append(int(round(np.mean(neighboursClass))))
+        (Class,frequency) = np.unique(neighboursClass , return_counts=True)
+
+        predTestLabels.append(int(Class[np.argmax(frequency)]))
+
+
+
+
 
 
     return np.array(predTestLabels)
@@ -111,7 +149,7 @@ def knn(train_set, train_labels, test_set, k, **kwargs):
     train_set_R = train_set[:, [6,9]]
     test_set_R = test_set[:, [6,9]]
 
-    predictions = KnnNeigh(train_set_R, train_labels, test_set_R, k)
+    predictions = KnnNeigh(train_set_R, train_labels, test_set_R, k, 2)
     print(Acc(test_labels, predictions))
 
 
@@ -120,15 +158,98 @@ def knn(train_set, train_labels, test_set, k, **kwargs):
 
 
 def alternative_classifier(train_set, train_labels, test_set, **kwargs):
+
+    train_set_2=train_set[:,[6,9]]
+    test_set_2=test_set[:,[6,9]]
+    train_labels1=train_labels
+    train_labels2=train_labels
+    min1=min(train_set_2[:,0])
+    max1=max(train_set_2[:,0])
+    min2=min(train_set_2[:,1])
+    max2=max(train_set_2[:,1])
+    range1=(max1-min1)
+    range2=(max1-min2)
+    partition1=range1/3
+    partition2=range2/3
+    partition1a=min1+partition1
+    partition1b=min1+(2*partition1)
+    partition2a=min2+partition2
+    partition2b=min2+(2*partition2)
+    partition1i=[]
+    partition1ii=[]
+    partition1iii=[]
+    partition2i=[]
+    partition2ii=[]
+    partition2iii=[]
+    f1=train_set_2[:,0]
+    f2=train_set_2[:,1]
+    f11=test_set_2[:,0]
+    f22=test_set_2[:,1]
+    i=0
+    while i<125:
+        if f1[i]<=partition1a:
+            partition1i.append(train_labels1[i])
+        if partition1a<f1[i]<=partition1b:
+            partition1ii.append(train_labels1[i])
+        if f1[i]>partition1b:
+            partition1iii.append(train_labels1[i])
+        i+=1
+    j=0
+    while j<125:
+        if f2[j]<=partition2a:
+            partition2i.append(train_labels2[j])
+        if partition2a<f2[j]<=partition2b:
+            partition2ii.append(train_labels2[j])
+        if f2[j]>partition2b:
+            partition2iii.append(train_labels2[j])
+        j+=1
+    childlist=[partition1i,partition1ii,partition1iii,partition2i,partition2ii,partition2iii]
+    entr1i=entropy(partition1i)[3]
+    ass1i=entropy(partition1i)[2]
+    entr1ii=entropy(partition1ii)[3]
+    ass1ii=entropy(partition1ii)[2]
+    entr1iii=entropy(partition1iii)[3]
+    ass1iii=entropy(partition1iii)[2]
+    entr2i=entropy(partition2i)[3]
+    ass2i=entropy(partition2i)[2]
+    entr2ii=entropy(partition2ii)[3]
+    ass2ii=entropy(partition2ii)[2]
+    entr2iii=entropy(partition2iii)[3]
+    ass2iii=entropy(partition2iii)[2]
+    entropylist=[entr1i,entr1ii,entr1iii,entr2i,entr2ii,entr2iii]
+    assignmentlist=[ass1i,ass1ii,ass1iii,ass2i,ass2ii,ass2iii]
+    k=0
+    alt_predictions=[]
+    while k<53:
+        if partition2a<f22[k]<=partition2b:
+            alt_predictions.append(2)
+        elif f22[k]<=partition2a:
+            alt_predictions.append(2)
+        elif f11[k]>partition1b:
+            alt_predictions.append(1)
+        elif f11[k]<=partition1a:
+            alt_predictions.append(3)
+        elif partition1a<f11[k]<=partition1b:
+            alt_predictions.append(2)
+        elif f22[k]>partition2b:
+            alt_predictions.append(1)
+        k+=1
+    alt_predictions_array=np.array(alt_predictions)
+    """calculate_accuracy(test_labels,alt_predictions_array)"""
     # write your code here and make sure you return the predictions at the end of
     # the function
-    return []
+    return alt_predictions_array
 
 
 def knn_three_features(train_set, train_labels, test_set, k, **kwargs):
+    train_set_R = train_set[:, [6,9, 3]]
+    test_set_R = test_set[:, [6,9, 3]]
+
+    predictions = KnnNeigh(train_set_R, train_labels, test_set_R, k, 3)
     # write your code here and make sure you return the predictions at the end of
     # the function
-    return []
+    print(Acc(test_labels, predictions))
+    return predictions
 
 
 def Pca(trainSet, testSet):
@@ -177,11 +298,13 @@ def knn_pca(train_set, train_labels, test_set, k, n_components=2, **kwargs):
     #print(reducedTestSet)
     #print(reducedTrainSet)
 
-    predicted = KnnNeigh(reducedTrainSet, train_labels, reducedTestSet, k)
+    predicted = KnnNeigh(reducedTrainSet, train_labels, reducedTestSet, k, n_components)
 
-    #print(Acc(test_labels, predicted))
-    plt.scatter(reducedTrainSet[:,0], -reducedTrainSet[:,1])
-    plt.show()
+
+
+    print(Acc(test_labels, predicted))
+    #plt.scatter(reducedTrainSet[:,0], -reducedTrainSet[:,1])
+    #plt.show()
 
 
     return predicted
@@ -227,3 +350,24 @@ if __name__ == '__main__':
         print_predictions(prediction)
     else:
         raise Exception('Unrecognised mode: {}. Possible modes are: {}'.format(mode, MODES))
+
+
+"""
+Skeleton code for CW2 submission.
+We suggest you implement your code in the provided functions
+Make sure to use our print_features() and print_predictions() functions
+to print your results
+"""
+
+
+"""
+>>> Combine all code
+
+>>> mkae code pretty
+
+>>> Fix the path stuff
+
+
+>>> change argument thing
+
+"""
